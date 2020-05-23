a8384e630c25c8a2e243d37c9e222c3e7f04c211
HarrierHarrier <v.lunev.mail@gmail.com>
2020-05-23 16:36:37 +0300

    added news sites parsers

---
+++ getting_news/parsers.py
@@ -0,0 +1,197 @@
+from datetime import datetime
+import requests
+
+from bs4 import BeautifulSoup
+
+
+def get_html(url):
+    try:
+        response = requests.get(url)
+        response.raise_for_status()
+        return response.text
+    except (requests.RequestException, ValueError):
+        return None
+
+
+class M24_accidents(object):
+    def __init__(self):
+        self.url = 'https://www.m24.ru'
+
+    def get_feed(self):
+        html = get_html(self.url + '/tag/происшествия')
+        if html:
+            soup = BeautifulSoup(html, 'html.parser')
+            html_class = 'b-materials-list b-list_infinity'
+            news_found = soup.find('div', class_=html_class)
+            news_found = news_found.find('ul').find_all('li')
+            result_news = []
+            for item in news_found:
+                news = item.find('p', class_='b-materials-list__title')
+                news = news.find('a')
+
+                title = news.text.strip('\n\t')
+                link = self.url + news['href']
+                time = item.find('span').text
+                date = datetime.now().strftime('%d.%m.%Y')
+
+                result_news.append({
+                    'title': title,
+                    'link': link,
+                    'time': time,
+                    'date': date
+                })
+            return result_news
+        else:
+            return None
+
+    def get_post(self, url):
+        html = get_html(url)
+        if html:
+            soup = BeautifulSoup(html, 'html.parser')
+            # есть страницы с разной разметкой, except - страницы
+            #  с видео и парой абзацев
+            try:
+                html_class = 'js-mediator-article'
+                text_blocks = soup.find('div', class_=html_class)
+                text_blocks = text_blocks.find_all('p', class_='')
+            except AttributeError:
+                html_class = 'b-material-body'
+                text_blocks = soup.find('div', class_=html_class)
+                text_blocks = text_blocks.find_all('p', class_='')
+
+            news_text = ''
+            for block in text_blocks:
+                block = block.text.replace('\xa0\n', '')
+                news_text += block.strip('\n\t ') + '\n'
+            return news_text
+        else:
+            return None
+
+
+class Mosday_accidents(object):
+    def __init__(self):
+        self.url = 'http://mosday.ru/news'
+
+    def get_feed(self):
+        html = get_html(self.url + '/tags.php?accident')
+        if html:
+            soup = BeautifulSoup(html, 'html.parser')
+            news_found = soup.find('body').find('table', width="100%",
+                                                height="500", cellpadding="0",
+                                                cellspacing="0", border="0")
+            news_found = news_found.find('table', width="100%",
+                                         cellpadding="0", cellspacing="10",
+                                         border="0")
+            news_found = news_found.find('center')
+            style = "font-family:Arial;font-size:15px"
+            news_found = news_found.find('table', width="95%",
+                                         cellpadding="0", cellspacing="10",
+                                         border="0",
+                                         style=style)
+            news_found = news_found.find_all('font', face="Arial", size="2",
+                                             color="#666666",
+                                             style="font-size:13px")
+            result_news = []
+            for item in news_found:
+                title = item.find('a').text
+                link = self.url + '/' + item.find('a')['href']
+                date = item.find('b').text
+                time = item.text[12:16]
+                result_news.append({'title': title,
+                                    'link': link,
+                                    'time': time,
+                                    'date': date})
+            return result_news
+        else:
+            return None
+
+    def get_post(self, url):
+        html = get_html(url)
+        if html:
+            soup = BeautifulSoup(html, 'html.parser')
+            text_blocks = soup.find('table', width="100%", cellpadding="0",
+                                    cellspacing="10", border="0")
+            text_blocks = text_blocks.find('article', itemprop="articleBody")
+            text_blocks = text_blocks.find('div', itemprop="text")
+            text_blocks = text_blocks.find_all('p')
+
+            news_text = ''
+            for block in text_blocks:
+                block = block.text.replace('\xa0\n', '')
+                news_text += block.strip('\n\t ') + '\n'
+            return news_text
+        else:
+            return None
+
+
+class VM_accidents(object):
+    def __init__(self):
+        self.url = 'https://vm.ru'
+
+    def get_feed(self):
+        html = get_html(self.url + '/accidents')
+        if html:
+            soup = BeautifulSoup(html, 'html.parser')
+            news_found = soup.find('div', class_='articles-list')
+            html_class = 'articles-list__item'
+            news_found = news_found.find_all('div', class_=html_class)
+            result_news = []
+            for item in news_found:
+                title = item.find('a').text.replace('\xa0\n', '')
+                title = title.strip('\n\t ')
+
+                link = self.url + item.find('a')['href']
+
+                published = item.find('ul')
+                html_class = 'articles-list__info articles-list__info--time'
+                time = published.find('li', class_=html_class).text
+                time = time.strip('\n\t')
+
+                html_class = 'articles-list__info articles-list__info--date'
+                day = published.find('li', class_=html_class).text
+                day = str(int(day.strip('\n\t')[0:2]))
+                month_and_year = datetime.now().strftime('.%m.%Y')
+                date = day + month_and_year
+
+                result_news.append({
+                    'title': title,
+                    'link': link,
+                    'time': time,
+                    'date': date
+                })
+            return result_news
+        else:
+            return None
+
+    def get_post(self, url):
+        html = get_html(url)
+        if html:
+            soup = BeautifulSoup(html, 'html.parser')
+            html_class = 'single-article__main-content'
+            text_blocks = soup.find('div', class_=html_class).find_all('p')
+            text_blocks = text_blocks[:-1]
+            news_text = ''
+            for block in text_blocks:
+                block = block.text.replace('\xa0', '').strip('\n\t ')
+                news_text += block + '\n'
+            return news_text
+        else:
+            return None
+
+
+if __name__ == "__main__":
+    parser = VM_accidents()
+    feed = parser.get_feed()
+
+    for news in feed:
+        print('----------------------')
+        print(news)
+        print()
+        text = parser.get_post(news['link'])
+        if text:
+            if len(text) > 250:
+                print(text)
+            else:
+                print('Полный текст новости в источнике.')
+    print('----------------------')
+    print(len(feed))
