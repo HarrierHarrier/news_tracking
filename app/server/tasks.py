fd454cfce9a714ee7cad0992d3c1d86978207cc1
HarrierHarrier <v.lunev.mail@gmail.com>
2020-05-26 13:13:14 +0300

    SQLite connected

--- app/server/tasks.py
+++ app/server/tasks.py
@@ -4,6 +4,8 @@ from natasha import MorphVocab, AddrExtractor
 
 from parsers import M24_accidents, Mosday_accidents, VM_accidents
 from utils import get_news
+from app import db
+from app.models import News
 
 
 def main():
@@ -31,6 +33,25 @@ def main():
         published = item['time'] + ' ' + item['date']
         published = datetime.strptime(published, '%H:%M %d.%m.%Y')
 
+        record = News(
+            title=item['title'],
+            link=item['link'],
+            date_and_time=published,
+            text=item['text'],
+            address=item['location']['address'],
+            street=item['location']['street'],
+            lat=item['location']['coordinates'][0],
+            lon=item['location']['coordinates'][1]
+        )
+        record_in_db = News.query.filter_by(link=item['link']).first()
+        if record_in_db:
+            continue
+        else:
+            db.session.add(record)
+    db.session.commit()
+
+    '''
+        # вывод записи
         print('-------------------------------------------------')
         print('title: ', item['title'])
         print('link: ,', item['link'])
@@ -42,24 +63,8 @@ def main():
         print('lon: ', item['location']['coordinates'][1])
     print('-------------------------------------------------')
     print(len(news_list))
+    '''
 
-    '''
-    for item in news_list:
-        if find_address_in_news(item, extractor):
-            record = {
-                'title': item['title'],
-                'link': item['link'],
-                'date_and_time': datetime.strptime(item['time'] + ' ' + item['date'], '%H:%M %d.%m.%Y'),
-                'text': item['text'],
-                'address': item['location']['address'],
-                'street': item['location']['street'],
-                'lat': item['location']['coordinates'][0],
-                'lon': item['location']['coordinates'][1]
-            }
-            print(record)
-            #db.session.add(record)
-            #db.session.commit()
-    '''
 
 if __name__ == "__main__":
     main()
